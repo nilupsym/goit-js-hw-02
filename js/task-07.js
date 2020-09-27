342845cf1a3fb78c9513410fb3b4e704db427ee4
Andriy Rudkovsky <a.rudkovskyy@gmail.com>
2020-09-27 15:19:56 +0300

    додає розв'язання задачі 2-8

--- js/task-07.js
+++ js/task-07.js
@@ -3,7 +3,7 @@ function filterArray(array) {
   const numbers = [];
   for (let i = 0; i < array.length; i += 1) {
     // Write code under this line
-    if (Number.isFinite(array[i]) === true) {
+    if (Number.isFinite(array[i])) {
       numbers.push(array[i]);
     }
   }
